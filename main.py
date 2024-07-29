47e01a5a1e0d3ddd96d5cb568536ec7561e1a166
bartw <bartwijman@gmail.com>
2024-07-29 14:24:35 +0200

    basic without interaction GUI 16

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from functions import get_todos, write_todos
 import time
 
 now = time.strftime("%b %d, %Y, %H:%M:%S")
-print ("It is", now)
+print("It is", now)
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
